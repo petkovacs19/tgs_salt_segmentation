a3cf4217dab9da42d9f706f0ef4b76b8802a6c26
Peter Kovacs <petkovacs19@gmail.com>
2018-12-10 11:20:49 -0500

    Generating folds with links to train/val set

--- tgs_preprocess.py
+++ tgs_preprocess.py
@@ -6,16 +6,17 @@ import shutil
 import numpy as np
 
 
-def create_sym_links(files, fold_index):
+def create_sym_links(files, fold_index, part='train'):
     """
     Creates symlinks for files in folder 'folds'
     files - list files to create symlinks for
     fold_index - int - symlinks will be created in 'folds/fold_{}/'
+    part - str - subdir to place symlinks to/ train/val
     """
-    print("Creating symlinks for fold {} and files".format(fold_index))
+    print("Creating symlinks for fold {} and files for {} set".format(fold_index, part))
     for file in files:
-        os.symlink("{}/images/{}".format(args.data_path,file), 'folds/fold_{}/images/{}'.format(fold_index, file))
-        os.symlink("{}/masks/{}".format(args.data_path,file), 'folds/fold_{}/masks/{}'.format(fold_index, file))
+        os.symlink("{}/images/{}".format(args.data_path,file), 'folds/fold_{}/{}/images/{}'.format(fold_index, part, file))
+        os.symlink("{}/masks/{}".format(args.data_path,file), 'folds/fold_{}/{}/masks/{}'.format(fold_index, part, file))
 
 def generate_folds(folds):
     """
@@ -26,15 +27,16 @@ def generate_folds(folds):
     folds = pre.gen_k_folds(args.fold)
     files = pre.filenames
     for index,fold in enumerate(folds):
-        os.makedirs('folds/fold_{}/images/salt'.format(index))
-        os.makedirs('folds/fold_{}/masks/salt'.format(index))
-        create_sym_links(np.array(files)[fold[1]], index)
-        
-        
+        for elem in ['train', 'val']:
+            os.makedirs('folds/fold_{}/{}/images/salt'.format(index, elem))
+            os.makedirs('folds/fold_{}/{}/masks/salt'.format(index, elem))
+        create_sym_links(np.array(files)[fold[1]], index, 'val')
+        create_sym_links(np.array(files)[fold[0]], index, 'train')
+             
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description = 'This is to split training data using k-fold validation. This script will generate folders for each fold and create symlinks to the given subset of dataset')
     parser.add_argument('--fold', type=int, help='Number of folds', default=5)
-    parser.add_argument('--data_path', type=str, help='Path to data', default='/home/pkovacs/tsg/data/val')
+    parser.add_argument('--data_path', type=str, help='Path to data', default='/home/pkovacs/tsg/data/all')
     args = parser.parse_args()
     print("==================================================")
     print("Generating {} folds of the data".format(args.fold))
